f1016dc9954a917573ef84924876b082966f35e9
shubham0419 <shubhamsc704707@gmail.com>
2025-08-19 15:47:41 +0530

    admin auth & cookie

---
+++ lecture20_Auth_Cookie_Admin/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+const path = require("path");
+require("dotenv").config();
+// router
+const authRouter = require("./routes/auth.route");
+const connectToDB = require("./connectDB/connect");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname,"public")));
+
+// routes
+app.use("/auth",authRouter);
+
+app.get("/", (req, res) => {	});
+
+connectToDB().then(()=>{
+  app.listen(PORT, () => console.log("Server running on port " + PORT));
+}).catch((error)=>console.log(error))
---
+++ lecture20_Auth_Cookie_Admin/middleware/auth.middleware.js
@@ -0,0 +1,15 @@
+const jwt = require("jsonwebtoken");
+
+function verifyUser(req,res,next){
+  try {
+    const token = req.headers.authorization;
+    const payload = jwt.verify(token,process.env.JWT_SECRET);
+    // payload = payload which we passed when creating token
+    req.user = payload;
+    next()
+  } catch (error) {
+    return res.status(400).json({message:error.message});
+  }
+}
+
+module.exports = verifyUser
