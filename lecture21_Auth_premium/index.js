3dce82b87a2e6535059f7aec3ff0420626a376e1
shubham0419 <shubhamsc704707@gmail.com>
2025-09-16 15:46:55 +0530

    react done

--- lecture21_Auth_premium/index.js
+++ lecture21_Auth_premium/index.js
@@ -6,13 +6,20 @@ require("dotenv").config();
 const cors = require("cors");
 
 var corsOptions = {
-  "origin": "http://localhost:3000",
-  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-  "preflightContinue": false,
-  "optionsSuccessStatus": 204
-}
+  origin: "http://localhost:3000",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  credentials: true
+};
 
-app.use(cors(corsOptions))
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  credentials: true
+}));
 // router
 const authRouter = require("./routes/auth.route");
 const userRouter = require("./routes/user.route");
@@ -20,14 +27,16 @@ const connectToDB = require("./connectDB/connect");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 // routes
-app.use("/auth",authRouter);
-app.use("/user",userRouter);
+app.use("/auth", authRouter);
+app.use("/user", userRouter);
 
-app.get("/", (req, res) => {	});
+app.get("/", (req, res) => {});
 
-connectToDB().then(()=>{
-  app.listen(PORT, () => console.log("Server running on port " + PORT));
-}).catch((error)=>console.log(error))
+connectToDB()
+  .then(() => {
+    app.listen(PORT, () => console.log("Server running on port " + PORT));
+  })
+  .catch((error) => console.log(error));
--- lecture21_Auth_premium/middleware/auth.middleware.js
+++ lecture21_Auth_premium/middleware/auth.middleware.js
@@ -1,16 +1,17 @@
 const jwt = require("jsonwebtoken");
 
-function verifyUser(req,res,next){
+async function verifyUser(req,res,next){
   try {
     const authorization = req.headers.authorization;
-    const token = authorization.split(" ")[1];
+    const token = authorization?.split(" ")[1] || req.cookies?.token;
     const payload = jwt.verify(token,process.env.JWT_SECRET);
-    // payload = payload which we passed when creating token
     req.user = payload;
-    next()
+    next();
   } catch (error) {
-    return res.status(400).json({message:error.message});
+    // res.redirect("/auth/login");
+    console.log(error);
+    res.status(400).json({message:error.message})
   }
 }
 
-module.exports = verifyUser
+module.exports = verifyUser;
